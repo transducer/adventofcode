f2d8eae29711e2dd3d422eb201ca29698a07bd0b
Erwin Rooijakkers <erwin.rooijakkers@mpare.net>
2016-12-25 00:10:16 +0100

    add logging to and update info in project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject adventofcode "0.1.0-SNAPSHOT"
+(defproject adventofcode "1.0.0"
   :description "Advent of Code 2016"
-  :url "http://example.com/FIXME"
+  :url "https://adventofcode.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [digest "1.4.5"]
-                 [org.clojure/math.combinatorics "0.1.3"]]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 [com.taoensso/timbre "4.8.0"]]
   :main ^:skip-aot adventofcode.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
