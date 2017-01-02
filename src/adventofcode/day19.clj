28ed65351a57d54f28637c8a87fe8102e96bdc50
Erwin Rooijakkers <erwin.rooijakkers@mpare.net>
2017-01-02 23:58:45 +0100

    day 19, part 1

---
+++ src/adventofcode/day19.clj
@@ -0,0 +1,42 @@
+(ns adventofcode.day19)
+
+(def num-elves 3005290)
+
+;; Does not perform, but can be used to find the maths
+#_(for [r (range 509 520)]
+  (do
+    (def num-elves r)
+
+    (defn finished? [d]
+      (= (count (remove zero? d)) 1))
+
+    (defn double [d]
+      (into [] (concat d d)))
+
+    (defn find-winner [d pos]
+      (cond (finished? d)   (reduced (inc (.indexOf d 1)))
+            (zero? (d pos)) d
+            :else           (assoc d (mod (+ (.indexOf (drop (inc pos) (double d)) 1) (inc pos)) num-elves)
+                                   0)))
+
+
+    (println ";;" num-elves (str (reduce find-winner (into [] (repeat num-elves 1)) (cycle (range num-elves)))))))
+
+
+;; Starting at place 8 with 1 we have previous + 2 or start with 1 again if value exceeds index...
+
+
+;; Part 1
+
+(-> (reduce #(let [prev (second (peek %1))
+                   next (+ prev 2)]
+               (if (<= next %2)
+                 (conj %1 [%2 next])
+                 (conj %1 [%2 1])))
+            [[8 1]]
+            (range (inc num-elves)))
+    peek
+    second)
+
+
+;; Part 2
