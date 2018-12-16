a5b6a768bfd5d6180ba77d27ef5e13848273261d
Erwin Rooijakkers <erwin@rooijakkers.software>
2018-12-16 22:50:51 +0100

    Initial day 7 without trying steps that aren't in the keys

--- src/adventofcode/2018/day7.clj
+++ src/adventofcode/2018/day7.clj
@@ -8,10 +8,37 @@
        string/split-lines
        (map #(re-seq #"[A-Z]" (string/replace-first % #"S" "")))
        (group-by second)
-       (map (fn [[k v]] [k (map first v)]))
+       (map (fn [[k v]] [k (into (sorted-set) (map first v))]))
        (into (sorted-map))))
 
-(defn available? [step step->dependencies]
-  )
+(defn available? [step->dependencies step]
+  (empty? (step->dependencies step)))
 
-;; Check available, do step
+(defn first-step [step->dependencies]
+  (->> (keys step->dependencies)
+       (apply disj (apply clojure.set/union (vals step->dependencies)))
+       first))
+
+(defn execute [step->deps step]
+  (dissoc
+   (->> step->deps
+        (map (fn [[s deps]] [s (disj deps step)]))
+        (into (sorted-map)))
+   step))
+
+
+;; Part 1
+
+(def start (first-step step->dependencies))
+
+(loop [step->deps (execute step->dependencies start)
+       acc start]
+  (if (empty? step->deps)
+    acc
+    (if-let [step (->> step->deps
+                    keys
+                    (filter #(available? step->deps %))
+                    first)]
+      (recur (execute step->deps step)
+             (str acc step))
+      acc)))
