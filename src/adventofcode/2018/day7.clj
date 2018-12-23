4ca8ba09c433fc6a950ff651dcf7ae1f8ce0c3a2
Erwin Rooijakkers <erwin@rooijakkers.software>
2018-12-23 13:16:56 +0100

    Day 7 part 2

--- src/adventofcode/2018/day7.clj
+++ src/adventofcode/2018/day7.clj
@@ -1,6 +1,6 @@
 (ns adventofcode.2018.day7
   (:require [clojure.java.io :as io]
-            [clojure.set :refer [union]]
+            [clojure.set :refer [difference union]]
             [clojure.string :as string]))
 
 (def step->dependencies
@@ -42,3 +42,77 @@
 (first (flatten (find-and-do-steps step->dependencies "")))
 
 ;; => CGKMUWXFAIHSYDNLJQTREOPZBV
+
+
+;; Part 2
+
+(defn steps-in-progress [s]
+  (->> (vals s)
+       (map :step)
+       (remove nil?)
+       set))
+
+(defn to-seconds [step]
+  (- (int (first (char-array step))) 4))
+
+(defn assign-worker [workers step]
+  (let [free-worker (->> workers
+                         (filter (fn [[w {:keys [seconds step]}]]
+                                   (zero? seconds)))
+                         ffirst)]
+    (assoc workers free-worker {:seconds (dec (to-seconds step)) :step step})))
+
+(defn dec-worker-time [workers]
+  (->> workers
+       (map (fn [[w {:keys [seconds step]}]]
+              (if (zero? seconds)
+                [w {:seconds 0 :step step}]
+                [w {:seconds (dec seconds) :step step}])))
+       (into {})))
+
+(defn free-worker-count [workers]
+  (->> workers
+       (filter (fn [[w {:keys [seconds]}]] (zero? seconds)))
+       count))
+
+(def counter (atom 0))
+
+(defn execute-finished-steps [step->deps workers]
+  (let [finished-workers (->> workers
+                              (filter (fn [[w {:keys [seconds step]}]]
+                                        (and (zero? seconds) step))))
+        finished-steps (->> finished-workers
+                            (map (fn [[k {:keys [step]}]] step)))]
+    (reduce
+     (fn [s->d step]
+       (execute s->d step))
+     step->deps
+     finished-steps)))
+
+(defn find-and-do-steps-with-workers [step->deps steps-stack seconds workers]
+  (let [step->deps (execute-finished-steps step->deps workers)]
+    (if (empty? step->deps)
+      seconds
+      (if (seq steps-stack)
+        (recur step->deps (pop steps-stack) seconds (assign-worker workers (peek steps-stack)))
+        (let [steps (->> (keys step->deps)
+                         (filter #(available? step->deps %))
+                         (set)
+                         (union (available-steps-in-deps step->deps))
+                         (#(difference % (steps-in-progress workers)))
+                         (into (sorted-set)))
+              steps-now (vec (take (free-worker-count workers) steps))]
+          (if (empty? steps-now)
+            (recur step->deps [] (inc seconds) (dec-worker-time workers))
+            (recur step->deps steps-now (inc seconds) (dec-worker-time workers))))))))
+
+(def workers
+  {:w1 {:seconds 0 :step nil}
+   :w2 {:seconds 0 :step nil}
+   :w3 {:seconds 0 :step nil}
+   :w4 {:seconds 0 :step nil}
+   :w5 {:seconds 0 :step nil}})
+
+(find-and-do-steps-with-workers step->dependencies [] 0 workers)
+
+;; => 1046
