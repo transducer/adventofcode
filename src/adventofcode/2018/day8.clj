134bdc2268fe30ced3c1e9a1b54d6ae7f536942d
Erwin Rooijakkers <erwin@rooijakkers.software>
2018-12-30 01:15:53 +0100

    Day 8 part 2

--- src/adventofcode/2018/day8.clj
+++ src/adventofcode/2018/day8.clj
@@ -4,15 +4,19 @@
 (def input
   (->> "resources/2018/day8.txt" slurp (format "[%s]") read-string))
 
-(def zipper
+(defn root
+  "Zips all the way up and returns the zipper at the root."
+  [loc]
+  (->> (iterate z/up loc) (take-while (complement nil?)) last))
+
+(def tree-with-zipper
   (loop [loc              (z/vector-zip [[]])
          [x & xs :as all] input]
-    (if (empty? all)
-      (z/root loc)
+    (if (seq all)
       (let [next-loc                              (z/next loc)
             {:keys [meta-data-cnt] :as next-node} (z/node next-loc)]
         (cond (z/branch? next-loc)
-              (recur (-> (iterate #(z/insert-child % []) (z/next loc))
+              (recur (-> (iterate #(z/insert-child % []) next-loc)
                          (nth x)
                          (z/insert-child :add-meta-data-cnt))
                      xs)
@@ -28,12 +32,13 @@
                      xs)
 
               :else
-              (recur next-loc all))))))
+              (recur next-loc all)))
+      (root loc))))
 
 
 ;; Part 1
 
-(->> zipper
+(->> tree-with-zipper
      flatten
      (mapcat :meta-data)
      (apply +))
@@ -42,3 +47,27 @@
 
 
 ;; Part 2
+
+(defn count-children
+  "Gets children count of loc. Not counting meta data map as a child."
+  [loc]
+  (try (-> loc z/children count dec)
+       (catch Exception _ 0)))
+
+(defn values-of-loc [loc]
+  (if-not loc
+    0
+    (let [meta-data (-> loc z/node last :meta-data)]
+      (if (zero? (count-children loc))
+        (apply + meta-data)
+        (for [i meta-data]
+          (let [child (nth (iterate z/right (z/next loc)) (dec i))]
+            (values-of-loc child)))))))
+
+(->> tree-with-zipper
+     z/next
+     values-of-loc
+     flatten
+     (apply +))
+
+;; 25656
