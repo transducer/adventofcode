2f031401959312aab207d7685661b903089fa338
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-11-15 10:48:14 +0100

    wip wip wip

--- src/adventofcode/2018/day9.clj
+++ src/adventofcode/2018/day9.clj
@@ -1,28 +1,33 @@
 (ns adventofcode.2019.day9)
 
-(def players 459)
-(def last-marble 71320)
+;; (def players 459)
+;; (def last-marble 71320)
 
-(def players 10)
-(def last-marble 1618)
+(def player-count 9)
+(def last-marble 25)
+
+(defn make-score [player-count]
+  (->> player-count
+       range
+       (mapcat (fn [p] [p 0]))
+       (apply hash-map)))
+
+(def board
+  {:score (make-score player-count)
+   :marbles [0]
+   :current-marble 0})
+
+(defn insert [{:keys [players marbles current-marble] :as board}]
+  ;; when multiple of 23 ...
+  ;; between 1, 2 clockwise
+  )
+
+;; (def players 10)
+;; (def last-marble 1618)
 ;; 8317
 
 
 ;; Part 1
 
-(loop [marble-count 0
-       current-marble 0
-       marbles (make-array Integer/TYPE last-marble)
-       scores (make-array Integer/TYPE players)]
-  (if (= marble last-marble)
-    (->> scores
-         (map-indexed vector)
-         (apply max-key second)
-         first)
-    (recur (inc marble-count)
-           (mod (inc current-marble) marble-count)
-           (aset scores 458 0)
-           (aget scores 458))
-    ))
 
 ;; =>
