684c1e2f3c328449db49c02b3c763debe8425b8a
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-12-05 22:00:33 +0100

    Day 4

--- project.clj
+++ project.clj
@@ -8,7 +8,9 @@
                  [org.clojure/math.combinatorics "0.1.3"]
                  [com.taoensso/timbre "4.8.0"]
                  [org.clojure/data.priority-map "0.0.7"]
-                 [fast-zip "0.7.0"]]
+                 [fast-zip "0.7.0"]
+                 [org.clojure/spec.alpha "0.2.176"]
+                 [org.clojure/test.check "0.10.0"]]
   :main ^:skip-aot adventofcode.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
