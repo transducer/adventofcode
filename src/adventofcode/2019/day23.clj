34803a8c481a6eea3c4a2afc8940a5ed5c1f8c3b
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-05-03 22:27:53 +0200

    Day 23 part 2

--- src/adventofcode/2019/day23.clj
+++ src/adventofcode/2019/day23.clj
@@ -1,6 +1,6 @@
 (ns adventofcode.2019.day23
   (:require [adventofcode.2019.intcode :refer [run-async]]
-            [clojure.core.async :as async :refer [chan alts!! <!! >!! timeout]]))
+            [clojure.core.async :as async :refer [chan alts!! <!! >!! timeout sliding-buffer]]))
 
 (def program
   (slurp "resources/2019/day23.txt"))
@@ -17,8 +17,8 @@
 (def network
   (into {}
         (for [network-address (range n-computers)
-              :let [in (chan 10)
-                    out (chan 10)
+              :let [in (chan 15)
+                    out (chan 15)
                     _ (boot! network-address in out)]]
           [network-address [in out]])))
 
@@ -39,3 +39,45 @@
       (recur (mod (inc sending-address) (dec n-computers))))))
 
 ;; => 22877
+
+
+;; Part 2
+
+(def address-zero-previous-y (atom nil))
+
+(def nat-address 255)
+(def nat (chan (sliding-buffer 2)))
+
+(loop [sending-address 0, idle-count 0, idle-reset? false]
+  (let [[in out] (network sending-address)
+        _ (when-not idle-reset? (>!! in -1))
+        [receiving-address _] (alts!! [(timeout 0) out])]
+    (if-not receiving-address
+      (if (= idle-count (* 2 n-computers))
+        (let [[in _] (network 0)
+              x (<!! nat)
+              y (<!! nat)]
+          (>!! in -1)
+          (>!! in x)
+          (>!! in y)
+          (recur 1 0 true))
+        (recur (mod (inc sending-address) (dec n-computers)) (inc idle-count) false))
+      (let [[in _] (network receiving-address)
+            x (<!! out)
+            y (<!! out)]
+        (cond (= receiving-address nat-address)
+              (do (prn y)
+                  (>!! nat x)
+                  (>!! nat y))
+              (zero? receiving-address)
+              (do (reset! address-zero-previous-y y)
+                  (>!! in -1)
+                  (>!! in x)
+                  (>!! in y))
+              :else
+              (do (>!! in -1)
+                  (>!! in x)
+                  (>!! in y)))
+        (recur (mod (inc sending-address) (dec n-computers)) 0 false)))))
+
+;; => 15210
