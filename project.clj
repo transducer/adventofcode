cabb1ebf2703b820a14b21e623c5942bfc2c7a76
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-12-03 06:43:32 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "https://adventofcode.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [digest "1.4.5"]
-                 [org.clojure/math.combinatorics "0.1.3"]
-                 [com.taoensso/timbre "4.8.0"]
-                 [org.clojure/data.priority-map "0.0.7"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [digest "1.4.9"]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [com.taoensso/timbre "5.1.0"]
+                 [org.clojure/data.priority-map "1.0.0"]
                  [fast-zip "0.7.0"]
-                 [org.clojure/spec.alpha "0.2.176"]
-                 [org.clojure/test.check "0.10.0"]
-                 [org.clojure/core.async "0.6.532"]
+                 [org.clojure/spec.alpha "0.2.187"]
+                 [org.clojure/test.check "1.1.0"]
+                 [org.clojure/core.async "1.3.610"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [quil "3.1.0"]
-                 [medley "1.2.0"]]
+                 [medley "1.3.0"]]
   :main ^:skip-aot adventofcode.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
