bbd71c9b83fefcd0b106dabb15d973ca60569e1d
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-12-06 08:13:03 +0100

    Day 6

---
+++ src/adventofcode/2020/day6.clj
@@ -0,0 +1,25 @@
+(ns adventofcode.2020.day6
+  (:require
+   [clojure.set :as set]
+   [clojure.string :as string]))
+
+(def input
+  (string/split (slurp "resources/2020/day6.txt") #"\R\R"))
+
+
+;; Part 1
+
+(->> input
+     (map (fn [s] (count (set (apply str (string/split s #"\r\n"))))))
+     (apply +))
+
+;; => 6625
+
+
+;; Part 2
+
+(->> input
+     (map (fn [s] (count (apply set/intersection (map set (string/split s #"\r\n"))))))
+     (apply +))
+
+;; => 3360
