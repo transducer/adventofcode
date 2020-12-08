b0d43b7c08cfb32c487181b0d3c3ea5f93b2f56b
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-12-08 06:59:24 +0100

    Day 8

---
+++ src/adventofcode/2020/day8.clj
@@ -0,0 +1,42 @@
+(ns adventofcode.2020.day8
+  (:require
+   [clojure.string :as string]))
+
+(defn parse [line]
+  (let [[op arg] (rest (re-find #"(\w+)\s([+-]\d+)" line))]
+    [op (read-string arg)]))
+
+(def instructions
+  (->> (slurp "resources/2020/day8.txt")
+       string/split-lines
+       (mapv parse)))
+
+
+;; Part 1
+
+(defn evaluate [instructions]
+  (loop [seen #{} i 0 acc 0]
+    (cond (seen i) {:acc acc :final-value nil}
+          (= i (dec (count instructions))) {:final-value acc}
+          :else (let [[op arg] (nth instructions i)]
+                  (recur (conj seen i)
+                         (if (= op "jmp") (+ i arg) (inc i))
+                         (if (= op "acc") (+ acc arg) acc))))))
+
+(:acc (evaluate instructions))
+
+;; => 1528
+
+
+;; Part 2
+
+(first
+ (keep-indexed
+  (fn [i [op arg]]
+    (case op
+      "nop" (-> instructions (assoc i ["jmp" arg]) evaluate :final-value)
+      "jmp" (-> instructions (assoc i ["nop" arg]) evaluate :final-value)
+      nil))
+  instructions))
+
+;; => 640
