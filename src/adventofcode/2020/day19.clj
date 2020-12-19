14dad63c3cd5438199e0ba9ede2d33565252351d
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-12-19 16:23:09 +0100

    Day 19

---
+++ src/adventofcode/2020/day19.clj
@@ -0,0 +1,33 @@
+(ns adventofcode.2020.day19
+  (:require
+   [clojure.string :as string]
+   [instaparse.core :as insta]))
+
+(def input
+  (string/split (slurp "resources/2020/day19.txt") #"\n\n"))
+
+(def grammar
+  (->> (first input)
+       string/split-lines
+       (sort-by (comp #(Integer. %) first #(string/split % #":")))
+       (string/join "\n")))
+
+(def messages
+  (string/split-lines (second input)))
+
+(def parser
+  (insta/parser grammar))
+
+(count (remove #(insta/failure? (parser %)) messages))
+;; => 176
+
+(def recursive-grammar
+  (-> grammar
+      (string/replace "8: 42" "8: 42 | 42 8")
+      (string/replace "11: 42 31" "11: 42 31 | 42 11 31")))
+
+(def recursive-parser
+  (insta/parser recursive-grammar))
+
+(count (remove #(insta/failure? (recursive-parser %)) messages))
+;; => 352
